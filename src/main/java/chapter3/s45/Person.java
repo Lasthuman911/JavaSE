83c4cf28ced53a9694aa83055c85401fd994f539
zhiming.wu <zhiming.wu@bqjr.cn>
2017-09-08 10:37:17 +0800

    equals 的同时 复写 hashCode方法，推荐使用apache的开源的hash码生成器

--- src/main/java/chapter3/s45/Person.java
+++ src/main/java/chapter3/s45/Person.java
@@ -1,11 +1,16 @@
 package chapter3.s45;
 
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 复写equals：此例违反了自反性，解决方式：去掉trim
  * test2：equals 要做null检查
+ * test3：一定要同时重写equals 和 hashCode方法
  * Created by zhiming.wu on 2017/9/8.
  */
 public class Person {
@@ -45,6 +50,7 @@ public class Person {
 
     /**
      * 改为getClass 进行判断
+     *
      * @param obj
      * @return
      */
@@ -62,9 +68,31 @@ public class Person {
         return false;
     }
 
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(name).toHashCode();//apache 开源的hash码生成器
+    }
+
     public static void main(String[] args) {
         //test1();
-        testNull();
+        //testNull();
+        Map<Person, Object> map = new HashMap<Person, Object>() {
+            {
+                put(new Person("zhangsan"), new Object());
+            }
+        };
+
+        List<Person> list = new ArrayList<Person>(){
+            {
+                add(new Person("zhangsan"));//匿名内部类
+            }
+        };
+
+        //列表是否包含
+        System.out.println(list.contains(new Person("zhangsan")));
+        //map 中是否包含
+        System.out.println(map.containsKey(new Person("zhangsan")));//未重写hashCode导致结果为false
+
     }
 
     private static void testNull() {
