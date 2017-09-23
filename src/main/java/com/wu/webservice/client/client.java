911b6bdf7dc7bce9cd8db865828c8c681564a764
lszhen911 <1078169344@qq.com>
2017-09-23 19:04:20 +0800

    add

--- src/main/java/com/wu/webservice/client/client.java
+++ src/main/java/com/wu/webservice/client/client.java
@@ -1,14 +1,16 @@
 package com.wu.webservice.client;
 
+import com.wu.webservice.service.HelloWorldImpl;
+
 /**
  * 客户端调用
  * Created by zhiming.wu on 2017/9/20.
  */
 public class client {
     public static void main(String[] args) {
-        HelloWorldImplService helloWorldImplService = new HelloWorldImplService();
+       /* HelloWorldImplService helloWorldImplService = new HelloWorldImplService();
         HelloWorldImpl helloWorld = helloWorldImplService.getHelloWorldImplPort();
         String resultStr = helloWorld.sayHello("lszhen");
-        System.out.println(resultStr);
+        System.out.println(resultStr);*/
     }
 }
